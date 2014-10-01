2f2965281164abdc8f5ca51fcf3adba4e4d4f754
Victor L. Senior <betselection@gmail.com>
2014-10-01 07:36:26 -0430

    Possible v0.1

--- Dozen_47_Column__Lw__Tracker/Properties/AssemblyInfo.cs
+++ Dozen_47_Column__Lw__Tracker/Properties/AssemblyInfo.cs
@@ -28,7 +28,6 @@ using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
-
 [assembly: AssemblyTitle("Dozen_47_Column__Lw__Tracker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -41,12 +40,9 @@ using System.Runtime.CompilerServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
+// [assembly: AssemblyDelaySign(false)]
+// [assembly: AssemblyKeyFile("")]
